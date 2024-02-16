959d5b3bc0ea7c0b5e458df6d2929116e73212ce
Ricard Roberg <ricardroberg@gmail.com>
2024-02-16 19:21:35 -0300

    Adicionado rascunho da classe Diretor

--- src/Diretor.java
+++ src/Diretor.java
@@ -1,4 +1,32 @@
-package src;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
-public class Diretor {
+public class Diretor extends Pessoa {
+    private int numeroFilmesDirigidos;
+
+    public Diretor(String nome, LocalDate dataNascimento, int numeroFilmesDirigidos) {
+        super(nome, dataNascimento);
+        this.numeroFilmesDirigidos = numeroFilmesDirigidos;
+    }
+
+    public int getNumeroFilmesDirigidos() {
+        return numeroFilmesDirigidos;
+    }
+
+    public void setNumeroFilmesDirigidos(int numeroFilmesDirigidos) {
+        this.numeroFilmesDirigidos = numeroFilmesDirigidos;
+    }
+
+    public static Diretor buscarDiretor(ArrayList<Diretor> diretores, String nomeDiretor) {
+        for (Diretor diretor : diretores) {
+            if (diretor.getNome().equalsIgnoreCase(nomeDiretor)) {
+                return diretor;
+            }
+        }
+        return null;
+    }
+
+    public static void cadastrarDiretor(ArrayList<Diretor> diretores, Diretor diretor) {
+        diretores.add(diretor);
+    }
 }
