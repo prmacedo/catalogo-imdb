d2e37e04ef79cf69cabb652e20767c2875726688
rafaelluis1 <rafael.luis.carvalho.maia@ccc.ufcg.edu.br>
2024-02-19 22:01:30 -0300

    implement class Actor

--- src/Ator.java
+++ src/Ator.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Ator extends Pessoa{
 
     private String cpf;
@@ -8,5 +10,35 @@ public class Ator extends Pessoa{
 
     }
 
+    public String getNome(){
+        return this.nome;
+    }
 
+    public String getCpf(){
+        return this.cpf;
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(nome,cpf);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (!(o instanceof Ator)) {
+            return false;
+        }
+        Ator ator = (Ator) o;
+
+        return this.getNome().equals(ator.getNome()) && this.getCpf().equals(ator.getCpf());
+    }
+
+    @Override
+    public String toString() {
+        return "Ator{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
 }
