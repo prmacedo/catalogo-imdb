ae46dd6619b7bd0cd46a2d19876ab8ed8d84a8ad
Paulo Macêdo <44329969+prmacedo@users.noreply.github.com>
2024-02-19 21:33:22 -0300

    create buscarFilme and cadastrarFilme methods

--- src/CatalogoIMDB.java
+++ src/CatalogoIMDB.java
@@ -1,3 +1,6 @@
+import utils.Validacao;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,5 +13,39 @@ public class CatalogoIMDB {
         this.filmes = new ArrayList<Filme>();
         this.atores = new ArrayList<Ator>();
         this.diretores = new ArrayList<Diretor>();
+    }
+
+    public void cadastrarFilme(String nome, LocalDate dataDeLancamento, Double orcamento, String descricao) {
+        boolean dadosSaoValidos =
+                Validacao.campoEhValido(nome) &&
+                Validacao.campoEhValido(dataDeLancamento) &&
+                Validacao.campoEhValido(orcamento) &&
+                Validacao.campoEhValido(descricao);
+
+        if (!dadosSaoValidos) {
+            throw new IllegalArgumentException("Dados inválidos, não foi possível cadastrar um novo filme!");
+        }
+
+        boolean filmeExiste = this.buscarFilme(nome, dataDeLancamento) != null;
+
+        if (filmeExiste) {
+            System.out.println("Filme já cadastrado!");
+            return;
+        }
+
+        Filme filme = new Filme(nome, dataDeLancamento, orcamento, descricao);
+        this.filmes.add(filme);
+    }
+
+    public Filme buscarFilme(String nome, LocalDate dataDeLancamento) {
+        Filme filmeBuscado = new Filme(nome, dataDeLancamento);
+
+        for (Filme filme : this.filmes) {
+            if (filme.equals(filmeBuscado)) {
+                return filme;
+            }
+        }
+
+        return null;
     }
 }
