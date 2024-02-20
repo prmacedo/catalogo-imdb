361abf6ae61bba379c3151d3b4a0ec78a9822777
Paulo Macêdo <44329969+prmacedo@users.noreply.github.com>
2024-02-19 21:17:32 -0300

    add utils package with Validacao class

---
+++ src/utils/Validacao.java
@@ -0,0 +1,20 @@
+package utils;
+
+import java.time.LocalDate;
+
+public class Validacao {
+    public static <T> boolean campoEhValido(T dado) {
+        if (dado == null) {
+            return false;
+        } else if (dado instanceof String dadoString) {
+            return !((String) dado).isEmpty();
+        } else if (dado instanceof LocalDate dadoDate) {
+            LocalDate dataDeReferencia = LocalDate.of(1888, 10, 14);
+            return !dadoDate.isBefore(dataDeReferencia);
+        } else if (dado instanceof Double dadoDouble) {
+            return !dadoDouble.isNaN() && dadoDouble >= 0d;
+        }
+
+        return false;
+    }
+}
